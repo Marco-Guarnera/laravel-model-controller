49ea1f23f589dcb7d30a982dce38546bdb7a2168
Marco-Guarnera <Darkaquon93@gmail.com>
2024-10-24 23:01:05 +0200

    Recovered data from database

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function index() {
+        $movies_list = Movie::all();
+        return view('movies.index', compact('movies_list'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Homepage
 Route::get('/', function () {
     return view('pages.home-page');
 })->name('homepage');
+
+// Movies
+Route::get('/movies', [MovieController::class, 'index'])->name('movies');
