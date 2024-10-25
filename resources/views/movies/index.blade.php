60cf49fca790a7057a9278aefe62519e250eb27d
Marco-Guarnera <Darkaquon93@gmail.com>
2024-10-25 09:55:54 +0200

    Completed Basic Structure

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,1 +1,24 @@
-@dd($movies_list)
+@extends("layouts.app")
+
+@section("page-title", "Movies")
+
+<!-- Movies -->
+@section("main")
+    <div class="container-fluid">
+        <div class="row g-3">
+            @forelse ($movies_list as $item)
+                <div class="col-2">
+                    <div class="card">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><h5 class="card-title">{{ $item->title }}</h5></li>
+                            <li class="list-group-item">Date: {{ $item->date }}</li>
+                            <li class="list-group-item">Vote: {{ $item->vote }}</li>
+                        </ul>
+                    </div>
+                </div>
+            @empty
+                <div>Not Available</div>
+            @endforelse
+        </div>
+    </div>
+@endsection
